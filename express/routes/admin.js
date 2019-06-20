ef4a82271059381c59f11957b779b42ec4b224a7
IamChevon <w2chevon@gmail.com>
2019-06-19 21:44:11 -0500

    removed dummy html. added path. serving html pages

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.use('/add-product', (req, res, next) => {
     console.log('add-product');    
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('first middleware');    
-    res.send('<p>Hello from home!</p>')
+    console.log(rootDir);    
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
