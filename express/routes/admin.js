e314b869a4c8bb1085b52754ddac2aef2b895e3d
IamChevon <w2chevon@gmail.com>
2019-06-19 20:24:57 -0500

    added route filtering

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.use('/add-product', (req, res, next) => {
     console.log('add-product');    
-    res.send('<form action="product" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>')
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>')
 });
 
 router.post('/product', (req, res, next) => {
