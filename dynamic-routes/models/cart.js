3a191cc54e9a324a5576f9ecdf0c8e0dd25dd121
IamChevon <w2chevon@gmail.com>
2019-07-10 04:22:22 -0500

    create cart class to create and manage cart data

---
+++ dynamic-routes/models/cart.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+
+    static addProduct(id, productPrice) {
+        //fetch cart
+        fs.readFile(p, (error, fileContent) => {
+
+            let cart = { products: [], totalPrice: 0 };
+            if (!error) {
+                cart = JSON.parse(fileContent);
+            }
+
+
+            //Analyze cart => find existing product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [ ...cart.products ];
+                console.log("updating product: ", updatedProduct);
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = { id: id, qty: 1 };
+                cart.products = [ ...cart.products, updatedProduct ];
+
+                console.log("creating product: ", updatedProduct);
+            }
+            //Add product, increment product quantity
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            });
+        });
+
+
+    }
+};
