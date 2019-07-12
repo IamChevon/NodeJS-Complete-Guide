0fdb6b94f481e5529a68867fe54945f2f981e5db
IamChevon <w2chevon@gmail.com>
2019-07-12 01:19:44 -0500

    add updated fetch, add and fetch all methods to shop mware

--- sql-introduction/controllers/shop.js
+++ sql-introduction/controllers/shop.js
@@ -2,35 +2,42 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
+  Product.fetchAll().then(([rows]) => {
+      res.render('shop/product-list', {
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
+  }).catch(err => {
+    console.log(err);
   });
+
 };
 
 exports.getProduct = (req, res, next) => {
 
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId).then(([product]) => {
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
-      path: '/prdoucts'
-    })
-  });
+      path: '/products'
+    });
+  }).catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    }).catch(err => {
+      console.log(err);
     });
-  });
+
 };
 
 exports.getCart = (req, res, next) => {
@@ -40,13 +47,13 @@ exports.getCart = (req, res, next) => {
       for (product of products) {
         const cartProductData = cart.products.find(prod => prod.id === product.id);
         if (cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty});
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: cartProducts 
+        products: cartProducts
       });
     });
 
@@ -67,7 +74,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
   });
-  
+
 };
 
 exports.getOrders = (req, res, next) => {
