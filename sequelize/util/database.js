6728667a8675a8c51fbbacdd3b011907ed7f6200
IamChevon <w2chevon@gmail.com>
2019-07-14 04:09:57 -0500

    refactored database config to follow sequelize structure

--- sequelize/util/database.js
+++ sequelize/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'iamchevon21'
+const sequelize = new Sequelize('node-complete', 'root', 'iamchevon21', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
