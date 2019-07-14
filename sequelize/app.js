d82bc62fb7c9be6e35e456bf9329460faea13d23
IamChevon <w2chevon@gmail.com>
2019-07-14 04:10:42 -0500

    import user and product. enforce relationships

--- sequelize/app.js
+++ sequelize/app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const Product = require('./models/product');
+
+const User = require('./models/user');
+
+const sequelize = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -20,5 +26,16 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+.sync({force: true})
+.then( result => {
+    // console.log(result);
+}).catch(err => {
+    console.log(err);
+});
 
 app.listen(3000);
